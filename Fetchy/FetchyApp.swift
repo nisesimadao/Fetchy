06d267c8bcc3d9a62429c5e78522c3ffa88fee04
opevista <opevista@gmail.com>
2026-01-23 15:31:04 +0900

    Initial Commit

---
+++ Fetchy/FetchyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchyApp.swift
+//  Fetchy
+//
+//  Created by Ueda Ritsu on 2026/01/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
